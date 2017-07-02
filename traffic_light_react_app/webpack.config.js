f78efbd1492d0589b05adde656a98edc6bafabfe
fimac <fimccawley@gmail.com>
2017-07-02 17:12:13 +1000

    updated webpack config, tested es6 to es5 compile ok

---
+++ traffic_light_react_app/webpack.config.js
@@ -0,0 +1,27 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + "/app/index.html",
+  filename: "index.html"
+});
+
+module.exports = () => {
+  return {
+    entry: ["./app/index.js"],
+    devtool: "source-map",
+    output: {
+      path: __dirname + "/dist",
+      filename: "index_bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.jsx?$/,
+          use: ["babel-loader"]
+        }
+      ]
+    },
+    plugins: [HTMLWebpackPluginConfig]
+  };
+};
